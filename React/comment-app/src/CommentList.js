67d9117cb0fb906ab17d756d2d2fdb2104f95c56
yuhengfdada <877751954@qq.com>
2021-07-06 13:37:13 +0800

    CommentInput

---
+++ React/comment-app/src/CommentList.js
@@ -0,0 +1,14 @@
+import {React, Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class CommentList extends Component {
+    render() {
+        return (
+            <div>
+                <span>CL</span>
+            </div>
+        )
+    }
+}
+
+export default CommentList;
